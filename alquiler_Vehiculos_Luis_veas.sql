0314c2deb23cdb0c708cc8f55061f81757bd8c94
LUISVEASXD <isaacveasvera@gmail.com>
2026-08-13 11:35:24 -0500

    Inserción de datps de prueba en contrato, detalle_contrato, mantenimiento, reserva y pagos

--- alquiler_Vehiculos_Luis_veas.sql
+++ alquiler_Vehiculos_Luis_veas.sql
@@ -115,3 +115,28 @@ values
     (3, 1, 'NYL-1111', 'KIA', 'RIO', 'disponbile'),
     (4, 2, 'LVV-1417', 'chevrolet', 'FTR 1624', 'disponible'),
     (6, 4, 'CAT-8680', 'Caterpillar', '320 GC', 'disponible');
+
+insert into contrato
+    (id_contrato, id_empleado, fecha_inicio,fecha_fin,estado)
+values
+    (1, 1, 1, '2026-08-01', '2026-08-05', 'finalizado'),
+    (2, 2, 2, '2026-08-10', '2026-08-15', 'activo')
+insert into detalle_Contrato
+    (id_detalle,id_contrato,id_vehiculo, dias_prestado, monto_prorrateado)
+values
+    (1, 1, 1, 4, 180.00),
+    (2, 2, 4, 5, 400.00);
+insert into mantenimiento
+    (id_mantenimient, id_vehiculo, fecha_inicio, fecha_fin, descripcion, costo, responsable)
+values
+    (1, 2, '2026-08-12', '2026-08-15', 'cambio de aceite y frenos', 150.00, 'Taller Auto S.A');
+insert into reserva
+    (id_reserva, id_cliente, id_vehiculo, fecha_reserva, monto_anticipado, estado)
+values
+    (1, 3, 6, '2026-08-11', 200.00, 'Confirmada');
+
+insert into pagos
+    (id_pago, id_contrato, id_reserva, monto, metodo_pago, fecha_pago)
+values
+    (1, 1, NULL, 180.00, 'Tarjeta de Crédito', '2026-08-01'),
+    (2, NULL, 1, 200.00, 'Transferencia', '2026-08-11');
